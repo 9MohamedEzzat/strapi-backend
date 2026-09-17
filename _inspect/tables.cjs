dd09bf59ed37cfa3bb01f3acc07edb143709082d
9MohamedEzzat <9MohamedEzzat@users.noreply.github.com>
2026-09-18 01:14:54 +0200

    remove scratch _inspect file

--- _inspect/tables.cjs
+++
@@ -1,6 +0,0 @@
-const Database = require('better-sqlite3');
-const path = 'C:/Users/MG/Projects/appointment_strapi/doctor_appointment/db/data.db';
-const db = new Database(path, { readonly: true });
-const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name").all().map(r => r.name);
-console.log('tables=' + tables.join(', '));
-db.close();
